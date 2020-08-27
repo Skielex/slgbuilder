786cbe0502b14765eb545da6bff4aeeafe571db0
Niels Jeppesen <niejep@dtu.dk>
2020-08-27 17:39:43 +0200

    Added builder for parallel QPBO.

--- slgbuilder/__init__.py
+++ slgbuilder/__init__.py
@@ -1,6 +1,7 @@
 from .bkbuilder import BKBuilder as MaxflowBuilder
 from .graphobject import GraphObject
 from .qpbobuilder import QPBOBuilder
+from .pqpbobuilder import PQPBOBuilder
 
 try:
     from .orbuilder import ORBuilder
